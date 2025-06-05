817219e257b62a273ea59d7527ea6d1ec2e7aab8
Arthur Muller <arty97muller@gmail.com>
2025-06-05 03:35:41 +0200

    Add back class person id logic
    I thought it was wrong, actually it does what it aims to do:
    it retrieves the autogenerated id of DATI_UTENTE and puts in the
    current instance of class, very usefull for later user in checking
    the id of that specific instance.

--- app/src/main/java/org/example/database/PersonalDataDAO.java
+++ app/src/main/java/org/example/database/PersonalDataDAO.java
@@ -13,18 +13,19 @@ import org.example.model.PersonalData;
  * Handles all database operations for DATI_ANAGRAFICI table
  */
 public class PersonalDataDAO {
-    
+
     /**
      * Create a new person in the database
-     * @param PersonalData  object to create
+     * 
+     * @param PersonalData object to create
      * @throws SQLException if database operation fails
      */
     public void createPerson(PersonalData person) throws SQLException {
         String sql = "INSERT INTO DATI_ANAGRAFICI (Nome , Cognome , Sesso , Telefono , Stato_residenza , Provincia , CAP, Via_Viale_Piazza, Numero_civico) VALUES (? , ? , ? , ? , ? , ? , ? , ? , ?)";
-        
+
         try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            
+                PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
             stmt.setString(1, person.getNome());
             stmt.setString(2, person.getCognome());
             stmt.setString(3, person.getSesso());
@@ -34,16 +35,16 @@ public class PersonalDataDAO {
             stmt.setString(7, person.getCap());
             stmt.setString(8, person.getVia());
             stmt.setString(9, person.getCivico());
-    
+
             int rowsAffected = stmt.executeUpdate();
-            
-            // if (rowsAffected > 0) {
-            //     try (ResultSet rs = stmt.getGeneratedKeys()) {
-            //         if (rs.next()) {
-            //             person.setPersonaId(rs.getInt(1));
-            //         }
-            //     }
-            // }
+
+            if (rowsAffected > 0) {
+                try (ResultSet rs = stmt.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        person.setPersonaId(rs.getInt(1));
+                    }
+                }
+            }
         }
     }
 }
