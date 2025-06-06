87462edc25f74543ad350b94705f814e372a8202
Arthur Muller <arty97muller@gmail.com>
2025-06-06 17:13:49 +0200

    Add Item model class
    basic implementation of item for the model, follows other impl of model
    classes like User etc.

---
+++ app/src/main/java/org/example/model/Item.java
@@ -0,0 +1,124 @@
+package org.example.model;
+
+import java.time.LocalDate;
+
+public class Item {
+    private int mediaId;
+    private int itemId;
+    private int inventoryId;
+    private String condizioni;
+    private String note;
+    private LocalDate data_acquisizione;
+
+    public Item() {
+    }
+
+    public Item(int mediaId, int inventoryId, String condizioni, String note, LocalDate data_acquisizione) {
+        this.mediaId = mediaId;
+        this.inventoryId = inventoryId;
+        this.condizioni = condizioni;
+        this.note = note;
+        this.data_acquisizione = data_acquisizione;
+    }
+
+    public Item(int mediaId, int itemId, int inventoryId, String condizioni, String note, LocalDate data_acquisizione) {
+        this.mediaId = mediaId;
+        this.itemId = itemId;
+        this.inventoryId = inventoryId;
+        this.condizioni = condizioni;
+        this.note = note;
+        this.data_acquisizione = data_acquisizione;
+    }
+
+    public int getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(int mediaId) {
+        this.mediaId = mediaId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(int inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public String getCondizioni() {
+        return condizioni;
+    }
+
+    public void setCondizioni(String condizioni) {
+        this.condizioni = condizioni;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public LocalDate getData_acquisizione() {
+        return data_acquisizione;
+    }
+
+    public void setData_acquisizione(LocalDate data_acquisizione) {
+        this.data_acquisizione = data_acquisizione;
+    }
+
+    @Override
+    public String toString() {
+        return "Item [mediaId=" + mediaId + ", itemId=" + itemId + ", inventoryId=" + inventoryId + ", condizioni="
+                + condizioni + ", note=" + note + ", data_acquisizione=" + data_acquisizione + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Item other = (Item) obj;
+        if (mediaId != other.mediaId)
+            return false;
+        if (itemId != other.itemId)
+            return false;
+        if (inventoryId != other.inventoryId)
+            return false;
+        if (condizioni == null) {
+            if (other.condizioni != null)
+                return false;
+        } else if (!condizioni.equals(other.condizioni))
+            return false;
+        if (note == null) {
+            if (other.note != null)
+                return false;
+        } else if (!note.equals(other.note))
+            return false;
+        if (data_acquisizione == null) {
+            if (other.data_acquisizione != null)
+                return false;
+        } else if (!data_acquisizione.equals(other.data_acquisizione))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(itemId);
+    }
+}
