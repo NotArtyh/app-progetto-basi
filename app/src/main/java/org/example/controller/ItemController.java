eb4c88dd159bde2f0d5576457dab30f7282e490c
Arthur Muller <arty97muller@gmail.com>
2025-06-06 17:59:13 +0200

    Add item controller for creating item logic
    interaction between view and model.
    Need to implement the checks on what variables are passed
    but it seems to be ok for now.
    
    it follows what the other creation method

---
+++ app/src/main/java/org/example/controller/ItemController.java
@@ -0,0 +1,33 @@
+package org.example.controller;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import org.example.database.ItemDAO;
+import org.example.model.Item;
+import org.example.view.UserView;
+
+public class ItemController {
+    private ItemDAO itemDAO;
+    private UserView itemView;
+
+    public ItemController(ItemDAO itemDAO, UserView itemView) {
+        this.itemDAO = itemDAO;
+        this.itemView = itemView;
+    }
+
+    public void createItem(int inventoryId, int mediaId, String condition, String note, LocalDate data_acquisizione) {
+        try {
+            Item item = new Item(mediaId, inventoryId, condition, note, data_acquisizione);
+
+            itemDAO.createItem(item);
+            itemView.displayMessage("User created successfully with ID: " + item.getItemId());
+            
+        } catch (SQLException e) {
+            itemView.displayError("Error creating user: " + e.getMessage());
+        } catch (Exception e) {
+            itemView.displayError("Unexpected error: " + e.getMessage());
+        }
+
+    }
+}
