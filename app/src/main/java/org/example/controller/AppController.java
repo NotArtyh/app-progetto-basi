7360bbfb1b868ac953dccaf78c8beb35b7376ec5
Arthur Muller <arty97muller@gmail.com>
2025-06-07 22:26:46 +0200

    Add AppController to handle interaction view / controllers
    The appcontroller is responsible for setting the event listeners
    and wiring the output of the pannels to the corresponding
    service + method combination, this way it only handles the wiring
    between what is visible and when to the actual operation without
    having any business logic inside.
    Just add the initialized controller to the constructor
    the pannel and its listeners and set the operation inside, finish
    by adding the corresponding pannel to the viewmanager with a
    suitable name.
    
    at the bottom the first page that is show at the app launch is setted
    it can be changed for debuggin purposes.

---
+++ app/src/main/java/org/example/controller/AppController.java
@@ -0,0 +1,57 @@
+package org.example.controller;
+
+import org.example.view.*;
+import org.example.view.panels.*;
+
+public class AppController {
+    private final ViewManager viewManager;
+    private final UserController userController;
+
+    public AppController(ViewManager viewManager, UserController userController) {
+        this.viewManager = viewManager;
+        this.userController = userController;
+        setupViews();
+    }
+
+    // prepares the action listeners for the signInPannel
+    // add new listeners as needed for each pannel
+    private void setupViews() {
+
+        // SignIn panel
+        SignInPanel signInPanel = new SignInPanel();
+        signInPanel.setActionListener(new SignInPanel.UserActionListener() {
+            public void onRegistration() {
+                viewManager.show("registration");
+            }
+
+            public void onLogin() {
+                viewManager.show("login");
+            }
+
+            public void onExit() {
+                System.exit(0);
+            }
+        });
+
+        // LogIn panel
+        LogInPanel logInPanel = new LogInPanel();
+        logInPanel.setActionListener(new LogInPanel.UserActionListener() {
+            public void onLoginSubmit(String username, String password) {
+                // Call the Usercontroller for handling the registration
+                userController.handleUserAuthentication(username, password);
+            }
+
+            public void onLoginCancel() {
+                viewManager.show("signIn"); // Go back to sign in
+            }
+        });
+
+        // Register all the pannels
+        viewManager.registerPanel("signIn", signInPanel);
+        viewManager.registerPanel("login", logInPanel);
+
+        // start the app on the signIn pannel
+        // here i should had the logic for displaying the two pannels side by side maybe
+        viewManager.show("signIn");
+    }
+}
