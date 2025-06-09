9fdfeba84e999dfce3661bde27297ac2a1f908f7
Arthur Muller <arty97muller@gmail.com>
2025-06-09 19:32:37 +0200

    Add SessionManager
    temporarly is at the same level as the App.java
    will probably move it to the service tab later but because
    the Session is a global thing for now it can stay here.

---
+++ app/src/main/java/org/example/SessionManager.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.example.model.User;
+
+/*
+ * Singleton class that tracks current session user
+ * Use this class when in need of knowing the curren user id
+ * for all types of operations
+ */
+public class SessionManager {
+    private static SessionManager instance;
+    private User currentUser;
+
+    private SessionManager() {
+        // private constructor to avoid it being generated elsewhere
+    }
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+
+    public void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
+
+    public User getCurrenUser() {
+        return currentUser;
+    }
+
+    public void clearSession() {
+        this.currentUser = null;
+    }
+
+    public boolean isUserLoggedIn() {
+        return currentUser != null;
+    }
+}
