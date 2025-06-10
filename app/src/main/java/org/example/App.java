31f7710b6f60be0e6bc1a0c925b627309e121ca3
Arthur Muller <arty97muller@gmail.com>
2025-06-10 03:44:12 +0200

    Initialize Item classes in app
    as we did for the user classes like its controller, the dao it uses
    and the service we do the same for the item, we provide everything
    it needs to operate, no repetition, we should reuse stuf we've already
    created, and pass it to the update appController

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -21,12 +21,18 @@ public class App {
             InventoryDAO inventoryDAO = new InventoryDAO();
             UserService userService = new UserService(userDAO, personalDataDAO, inventoryDAO);
 
+            ItemDAO itemDAO = new ItemDAO();
+            MediaDAO mediaDAO = new MediaDAO();
+            ItemService itemService = new ItemService(itemDAO, mediaDAO);
+
             // create the controllers
             UserController userController = new UserController(userService, viewManager);
 
+            ItemController itemController = new ItemController(itemService, viewManager);
+
             // Create the final App controller that manages everything
             // could have a return for validation but ok for now
-            AppController appController = new AppController(viewManager, userController);
+            AppController appController = new AppController(viewManager, userController, itemController);
 
             mainFrame.setVisible(true);
         });
