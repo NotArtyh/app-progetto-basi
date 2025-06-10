eb64d6ea870a652a4a42225b79a3f4d4f9c0a1e4
Arthur Muller <arty97muller@gmail.com>
2025-06-10 16:56:22 +0200

    Update invenotryService for moving inv initilization inside
    for the code separation of the project its better to move the
    needed initialization inside the service, initialize the inventory
    there and then handle everything the inventoryController

--- app/src/main/java/org/example/services/InventoryService.java
+++ app/src/main/java/org/example/services/InventoryService.java
@@ -1,7 +1,9 @@
 package org.example.services;
 
 import java.sql.SQLException;
+import java.util.List; // Maybe remove later used for this impl
 
+import org.example.SessionManager;
 import org.example.database.DAOResult;
 import org.example.database.InventoryDAO;
 import org.example.database.ItemDAO;
@@ -10,8 +12,14 @@ import org.example.model.Item;
 
 public class InventoryService {
     // Initialize all the needed DAOs
+    private InventoryDAO inventoryDAO;
+    private ItemDAO itemDAO;
 
     // Build the constructor with said DAOs
+    public InventoryService(InventoryDAO inventoryDAO, ItemDAO itemDAO) {
+        this.inventoryDAO = inventoryDAO;
+        this.itemDAO = itemDAO;
+    }
 
     /*
      * No createInventory method is implemented here because we wouldn't create
@@ -25,15 +33,23 @@ public class InventoryService {
     public ServiceResult getItemsInInvetory(int invenotryId) {
         try {
             // Implement the logic here
-            System.err.println("logica da implementare");
 
-            return new ServiceResult(true, "");
+            List<Item> items = itemDAO.getItemsByUserId(invenotryId); // temp
 
-        // } catch (SQLException e) {
-        //     return new ServiceResult(false, "Error: " + e.getMessage());
+            return new ServiceResult(true, ""); // temp
+        } catch (SQLException e) {
+            return new ServiceResult(false, "Error: " + e.getMessage());
         } catch (Exception e) {
             return new ServiceResult(false, "Unexpected error: " + e.getMessage());
         }
     }
 
+    public ServiceResult initializeInventory() {
+        try {
+
+            return new ServiceResult(false, null); // temp
+        } catch (Exception e) {
+            return new ServiceResult(false, "Unexpected error: " + e.getMessage());
+        }
+    }
 }
