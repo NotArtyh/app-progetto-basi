82cc74e369efc381dfd5219ddb6ee384c1a64a11
Arthur Muller <arty97muller@gmail.com>
2025-06-11 03:10:35 +0200

    Add AppController dev note

--- app/src/main/java/org/example/controller/AppController.java
+++ app/src/main/java/org/example/controller/AppController.java
@@ -180,4 +180,43 @@ public class AppController {
         } else
             viewManager.show("signIn");
     }
+
+    /*
+     * we have two main options for handling this dynamic pannel generation - now on
+     * refered to as "on demand pannels"
+     * 
+     * 1. we handle this pannels initialization in their respective controllers. so
+     * Invenotry would stand in the inventory controller, user logout and inso in
+     * user controller etc. - i prefer to keep everything here
+     * 
+     * 2. we keep everything here we could create a special "on deman init" method
+     * to handle this type of pannels all in one place and provide everything
+     * by simply calling this private method that handles all of them in one go
+     * - i'd prefer doing it this way, its mutch more coesive and compact, even
+     * if all the on demand methods are done togheter, like initializing the
+     * user inventory for the view, or initializing the user to be displayed
+     * and the likes, it makes sense to call all of them toghether as they all
+     * should happen if the user is logged in, not otherwise and we could handle
+     * the action listeners here.
+     * 
+     * IMPORTANT - here we should also integrate with the controllers to handle
+     * the methods for refrescing the data for the view.
+     * as of now that is handled by a public method in the view of sai pannel
+     * but it isn't a great solution in and off itself, because calling
+     * the dao directly inside the view isn't advised for the structure we've
+     * planned.
+     * 
+     * CONSIDER - maybe we shoudln't be handling those pannels this way, maybe
+     * we should just compose the base pannel, we create one pannel containing
+     * the buttons that will need static action listeners, like a logout button
+     * or a exit button for the inv view, and inside of each controller we
+     * recompose said pannel based on the new one after the service has passed
+     * it the needed data for the view, maybe a list of model objects with the
+     * wanted data inside. in order to do this we could add a new method to the
+     * view manager which handles updating already existing pannles by re mapping
+     * them to a existing name, like an updatePannel method which we would call
+     * every time we want to recompose the pannel, like the home pannel which is
+     * simply the composition of subpannels but in this case we have the logic
+     * for updating.
+     */
 }
