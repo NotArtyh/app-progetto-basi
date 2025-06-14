a92db7faa96a3dd14a96e2a15ab483ff448f4a27
Giuseppe Cattolico <giuseppe.cattolico@studio.unibo.it>
2025-06-14 12:09:58 +0200

    Minor changes

--- app/src/main/java/org/example/controller/UsersInventoryController.java
+++ app/src/main/java/org/example/controller/UsersInventoryController.java
@@ -4,16 +4,26 @@ import org.example.services.InventoryService;
 import org.example.services.UsersInventoryService;
 import org.example.view.DynamicPanelManager;
 import org.example.view.ViewManager;
+import org.example.view.panels.UsersInventoryPanel;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.example.SessionManager;
+import org.example.database.UserDAO;
 /**
  * Inventory Controller Class
  * Handles business logic and coordinates between Model and View
  */
+import org.example.model.User;
 public class UsersInventoryController {
     private InventoryService inventoryService;
     private ViewManager viewManager;
     private DynamicPanelManager dynamicPanelManager;
     private UsersInventoryService usersInventoryService;
+    private UserDAO userDAO;
+    private List<User> allUsers;
 
     public UsersInventoryController(UsersInventoryService usersInventoryService, ViewManager viewManager,
             DynamicPanelManager dynamicPanelManager) {
@@ -24,5 +34,49 @@ public class UsersInventoryController {
 
     public void handleUsersInventoryUpdate() {
 
+        try {
+
+            this.allUsers = userDAO.getAllUsersExceptCurrent(SessionManager.getInstance().getCurrenUser().getUserId());
+            System.out.println("Caricati " + allUsers.size() + " utenti");
+        } catch (SQLException e) {
+            System.err.println("Errore SQL: " + e.getMessage());
+            e.printStackTrace();
+            
+            this.allUsers = new ArrayList<>();
+
+        } catch (Exception e) {
+            System.err.println("Errore generico: " + e.getMessage());
+            e.printStackTrace();
+            
+            this.allUsers = new ArrayList<>();  
+        }
+    
+
+            /*  Validate if the service returned any items
+            if (!result.isSuccess()) {
+                System.out.println(result.getMessage());
+            }*/
+
+            // Pass the list of items to the view so that it can update via a special
+            // constructor that handles the updates.
+            // Reset the action listerners for the new pannel
+            UsersInventoryPanel updatedUsersInventoryPanel = new UsersInventoryPanel();
+           
+            /* 
+            updatedUsersInventoryPanel.setActionListener(new UsersInventoryPanel.UserActionListener() {
+                public void onTrade() {
+                    viewManager.show("trade"); va al panel di trade
+                }
+            });
+            */
+            
+            dynamicPanelManager.setusersInventoryPanel(updatedUsersInventoryPanel);
+            dynamicPanelManager.updateUsersInventoryPanel();
+
+       
+        }
     }
-}
+
+
+
+
