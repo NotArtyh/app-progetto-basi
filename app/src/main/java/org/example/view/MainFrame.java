6644473167d98c2ef25e7be78949372c28ca299b
Arthur Muller <arty97muller@gmail.com>
2025-06-15 19:47:37 +0200

    Change icon

--- app/src/main/java/org/example/view/MainFrame.java
+++ app/src/main/java/org/example/view/MainFrame.java
@@ -13,7 +13,7 @@ public class MainFrame extends JFrame {
     }
 
     private void initializeFrame() {
-        setTitle("👨‍🌾");
+        setTitle("💿");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
         setLocationRelativeTo(null);
