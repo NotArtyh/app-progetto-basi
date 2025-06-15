bc92d9908ae23307c279c8742b21ba98337aeadb
Arthur Muller <arty97muller@gmail.com>
2025-06-16 00:44:32 +0200

    Remove dev server credentials

--- app/src/main/java/org/example/database/DatabaseConnection.java
+++ app/src/main/java/org/example/database/DatabaseConnection.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    
-    private static final String URL = "jdbc:mysql://100.66.102.13:3306/BEL";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "muffin1234!";
+
+    private static final String URL = "jdbc:mysql://";  // server url - localhost if local server
+    private static final String USERNAME = "";          // user for the database
+    private static final String PASSWORD = "";          // password for that user
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
@@ -25,4 +25,3 @@ public class DatabaseConnection {
         }
     }
 }
-
